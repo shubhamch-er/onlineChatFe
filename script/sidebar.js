ff5adff1921ec7f9835f3967e64e487429799bfb
Yogeshwar Chaturvedi <kanhay8@gmail.com>
2021-05-24 21:20:35 +0530

    Added sidebar chat

--- script/sidebar.js
+++ script/sidebar.js
@@ -7,3 +7,31 @@ const fnSidebarName = () => {
 }
 
 fnSidebarName();
+
+//Sidebar friend list
+
+const fnSidebarFriendList = () => {
+    fetch(baseURL + "users")
+        .then(res => res.json())
+        .then(res => {
+
+            for (var i = 0; i < res.length; i++) {
+                document.querySelector('.sidebar__friend-list').insertAdjacentHTML(
+                    'afterbegin',
+                    `<div class="friend">
+                    <img src="../image/avatar6.png" alt="avatar" class="avatar">
+                    <div class="friend__info">
+                      <h1><span id="friend-name${i}"></span></h1>
+                      <p>Last message</p>
+                    </div>
+                  </div>`
+                )
+            }
+            for (var i = 0; i < res.length; i++) {
+                replaceInnerHTMLById("friend-name" + i, res[i].name);
+            }
+        })
+}
+
+
+fnSidebarFriendList();
