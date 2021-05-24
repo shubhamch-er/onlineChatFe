c541d6d015ed84efc67117483619183d480f4e82
shubhamch-er <shubhamch.er@gmail.com>
2021-05-24 12:56:48 +0530

    Adding update name functionality

--- script/profile.js
+++ script/profile.js
@@ -1,1 +1,17 @@
-alert("Loaded !")
+// All user profile update code goes here.
+
+/**
+ * @author Shubham Chaudhary
+ */
+
+const fnUpdateNameHandler = () => {
+  const userData = {
+    name: getValueById("new-name")
+  }
+  const URL = fnUserUrlBuilder("/name")
+  fetch(URL, updateProfile(userData))
+    .then(res => res.json())
+    .then(res => alert("Successfully changed profile"))
+    .then(() => redirectToProfilePage())
+    .catch(err => console.error(err));
+}
---
+++ script/utils.js
@@ -0,0 +1,26 @@
+
+const baseURL = "http://localhost:8080/api/";
+
+const getUserId = () => localStorage.getItem("userId");
+
+const getValueById = (id) => document.getElementById(id).value;
+
+const fnUserUrlBuilder = (param1) => {
+  const userId = getUserId();
+  return baseURL + "user/" + "10001" + param1;
+}
+
+const redirectToProfilePage = () => window.location.replace("http://localhost:5500/html/profile.html")
+
+
+const updateProfile = (userData) => {
+  const newData =
+  {
+    method: "PUT",
+    body: JSON.stringify(userData),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  return newData;
+}
