daff88a2fe2087832ed44ede84807a447bacf0b5
shubhamch-er <shubhamch.er@gmail.com>
2021-05-24 14:52:13 +0530

    Adding update profile functionality

--- script/profile.js
+++ script/profile.js
@@ -1,14 +1,56 @@
 // All user profile update code goes here.
 
-/**
- * @author Shubham Chaudhary
- */
-
 const fnUpdateNameHandler = () => {
   const userData = {
     name: getValueById("new-name")
   }
   const URL = fnUserUrlBuilder("/name")
+  fetch(URL, updateProfile(userData))
+    .then(res => res.json())
+    .then(res => alert("Successfully changed profile"))
+    .then(() => redirectToProfilePage())
+    .catch(err => console.error(err));
+}
+
+const fnUpdateEmailHandler = () => {
+  if (isEmailConfirmed()) {
+    const userData = {
+      email: getValueById("new-email")
+    }
+    const URL = fnUserUrlBuilder("/email")
+    fetch(URL, updateProfile(userData))
+      .then(res => res.json())
+      .then(res => alert("Successfully changed profile"))
+      .then(() => redirectToProfilePage())
+      .catch(err => console.error(err));
+  }
+  else {
+    alert("Email Do Not Match");
+  }
+}
+
+const fnUpdatePasswordHandler = () => {
+  if (isPasswordConfirmed()) {
+    const userData = {
+      password: getValueById("new-password")
+    }
+    const URL = fnUserUrlBuilder("/password")
+    fetch(URL, updateProfile(userData))
+      .then(res => res.json())
+      .then(res => alert("Successfully changed profile"))
+      .then(() => redirectToProfilePage())
+      .catch(err => console.error(err));
+  }
+  else {
+    alert("Password Do Not Match");
+  }
+}
+
+const fnUpdateDobHandler = () => {
+  const userData = {
+    dob: getValueById("new-dob")
+  }
+  const URL = fnUserUrlBuilder("/dob")
   fetch(URL, updateProfile(userData))
     .then(res => res.json())
     .then(res => alert("Successfully changed profile"))
---
+++ script/properties.js
@@ -0,0 +1,3 @@
+const baseURL = "http://localhost:8080/api/";
+
+const redirectToProfilePage = () => window.location.replace("http://localhost:5500/html/profile.html")
