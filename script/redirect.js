ea550722c14a145e250f1c972b1a4602a777084d
Rohit Bhatt <rohitbhatt2000@gmail.com>
2021-05-26 01:46:15 +0530

    changes in redirect.js

--- script/redirect.js
+++ script/redirect.js
@@ -14,7 +14,7 @@ const redirectToPostRequestPage = (i) => {
           window.location.replace("C:/Users/Rohit/Documents/Projects/onlineChatFe/html/post-request.html?" + userNum + "?" + 1)
         else {
           // alert("redirecting to chat.html");
-          window.location.replace("C:/Users/Rohit/Documents/Projects/onlineChatFe/html/chat.html")
+          window.location.replace("C:/Users/Rohit/Documents/Projects/onlineChatFe/html/chat.html?" + userNum + "?" + 1)
         }
       }
     });
